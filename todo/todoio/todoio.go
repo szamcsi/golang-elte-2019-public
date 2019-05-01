dc718cc83ec9b6c93cc1d7e0741ca18985eaa8e3
Wide <you@example.com>
2019-05-01 22:18:39 +0200

    first commit

--- todo/todoio/todoio.go
+++ todo/todoio/todoio.go
@@ -2,6 +2,10 @@
 package todoio
 
 import (
+	"encoding/csv"
+	"fmt"
+	"os"
+	"strconv"
 	"time"
 )
 
@@ -9,15 +13,53 @@ import (
 type Entry struct {
 	Done     bool
 	Text     string
-	Deadline time.Time // optional
+	Deadline time.Time
 }
 
 // LoadEntries imports TODO entries from a CSV file.
 func Load(path string) ([]*Entry, error) {
-	return nil, nil
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	lines, err := csv.NewReader(file).ReadAll()
+	if err != nil {
+		return nil, err
+	}
+	var entries []*Entry
+	for i, line := range lines {
+		if i == 0 {
+			continue
+		}
+		if len(line) != 3 {
+			return nil, fmt.Errorf("Line %d: wrong format error", i+1)
+		}
+		var entry Entry
+		entry.Done, err = strconv.ParseBool(line[0])
+		entry.Text = line[1]
+		entry.Deadline, err = time.Parse("2006-01-02", line[2])
+		if err != nil {
+			return nil, err
+		}
+		entries = append(entries, &entry)
+	}
+	return entries, nil
 }
 
 // StoreEntries exports TODO entries to a CSV file.
 func Store(path string, entries []*Entry) error {
+	file, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	file.WriteString("DONE,TEXT,DEADLINE\n")
+	for _, entry := range entries {
+		_, err := file.WriteString(strconv.FormatBool(entry.Done) + "," + entry.Text + "," + entry.Deadline.Format("2006-01-02") + "\n")
+		if err != nil {
+			return err
+		}
+	}
 	return nil
 }
--- todo/todoio/todoio_test.go
+++ todo/todoio/todoio_test.go
@@ -1,8 +1,20 @@
 package todoio
 
 import (
+	"fmt"
 	"testing"
+	"time"
+
+	"github.com/google/go-cmp/cmp"
 )
+
+func StrToDate(str string) time.Time {
+	date, err := time.Parse("2006-01-02", str)
+	if err != nil {
+		fmt.Errorf("Error: date in wrong format: %s", str)
+	}
+	return date
+}
 
 func TestLoad(t *testing.T) {
 	path := "../golang-todo.csv"
@@ -11,7 +23,24 @@ func TestLoad(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	want := []*Entry{}
+	want := []*Entry{
+		{false, "display entries from a CSV import", StrToDate("2019-04-04")},
+		{false, "web interface and CLI sent for review", StrToDate("2019-05-10")},
+	}
 
-	// TODO: implement the test
+	for i, line := range got {
+		if !cmp.Equal(line, want[i]) {
+			if line.Done != want[i].Done {
+				t.Errorf("Line %d: got = %t; want = %t", i+1, line.Done, want[i].Done)
+			}
+			if line.Text != want[i].Text {
+				t.Errorf("Line %d: got = %s; want = %s", i+1, line.Text, want[i].Text)
+			}
+			if !cmp.Equal(line.Deadline, want[i].Deadline) {
+				t.Errorf("Line %d: got = %s; want = %s", i+1, line.Deadline.Format("2006-01-02"), want[i].Deadline.Format("2006-01-02"))
+			}
+		}
+	}
+
+	//Store("../test.csv", got)
 }
